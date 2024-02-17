ff8acd5dc15406055f5758ec74750a97cb5353bb
Nanskip <nanskip.oof@gmail.com>
2024-02-17 03:52:58 +0200

    added project cuboid texts as comments

--- main.lua
+++ main.lua
@@ -1,23 +1,35 @@
+--[[
+ ____  ____   __     __  ____   ___  ____         ___  _  _  ____   __    __   ____ 
+(  _ \(  _ \ /  \  _(  )(  __) / __)(_  _)       / __)/ )( \(  _ \ /  \  (  ) (    \
+ ) __/ )   /(  O )/ \) \ ) _) ( (__   )(        ( (__ ) \/ ( ) _ ((  O )  )(   ) D (
+(__)  (__\_) \__/ \____/(____) \___) (__)        \___)\____/(____/ \__/  (__) (____/
+]]--
+
+-- only load everything from github
+
 Client.OnStart = function()
     loadGitHub()
 end
 
 Client.Tick = function(dt)
-    -- nothing
+ -- i can't implement tick as github file, so i should edit it here.
+ -- nothing yet.
 end
 
+-- load everything
 loadGitHub = function()
     loadFromGitHub("https://raw.githubusercontent.com/Nanskipp/project-cuboid/main/scripts/world_generator.lua",
     function(obj)
-        world_generator = obj()
+        world_generator = obj() -- set this as module
     end)
     loadFromGitHub("https://raw.githubusercontent.com/Nanskipp/project-cuboid/main/scripts/start.lua",
     function(obj)
         start = obj
-        start()
+        start() -- called once like Client.OnStart()
     end)
 end
 
+-- loading function
 loadFromGitHub = function(url, callback)
     HTTP:Get(url, function(res)
         if res.StatusCode ~= 200 then
@@ -29,3 +41,18 @@ loadFromGitHub = function(url, callback)
         callback(obj)
       end)
 end
+--[[
+  ____                     _                 _        ____           _               _       _ 
+ |  _ \   _ __    ___     (_)   ___    ___  | |_     / ___|  _   _  | |__     ___   (_)   __| |
+ | |_) | | '__|  / _ \    | |  / _ \  / __| | __|   | |     | | | | | '_ \   / _ \  | |  / _` |
+ |  __/  | |    | (_) |   | | |  __/ | (__  | |_    | |___  | |_| | | |_) | | (_) | | | | (_| |
+ |_|     |_|     \___/   _/ |  \___|  \___|  \__|    \____|  \__,_| |_.__/   \___/  |_|  \__,_|
+                        |__/                                                                     
+                                    __                _  __                    __     _        
+                                   / /   __ __       / |/ / ___ _  ___   ___  / /__  (_)   ___ 
+                                  / _ \ / // /      /    / / _ `/ / _ \ (_-< /  '_/ / /   / _ \
+                                 /_.__/ \_, /      /_/|_/  \_,_/ /_//_//___//_/\_\ /_/   / .__/
+                                     /___/                                              /_/    
+ Inspired by Project Zomboid game.
+
+]]--
